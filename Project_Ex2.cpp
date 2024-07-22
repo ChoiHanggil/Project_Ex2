1ecde8f934c5038cc30b6c21f1d6368c80dbd7f7
chj1997930@naver.com <chj1997930@naver.com>
2024-07-22 14:03:57 +0900

    완

--- Project_Ex2.cpp
+++ Project_Ex2.cpp
@@ -4,8 +4,62 @@
 #include<map>
 #include<string>
 #include<queue>
+#include<unordered_map>
+#include<unordered_set>
 //1
-
+////m*n + n*log n + m*n = m*n
+//int ConinChange(std::vector<int>& coins, const int amount)
+//{
+//	if (amount == 0)
+//	{
+//		return 0;
+//	}
+//
+//	std::vector<bool> table(amount + 1, false);
+//	table[0] = true;
+//
+//	for (int i = 0; i <= amount; ++i)
+//	{
+//		if (table[i])
+//		{
+//			for (auto e : coins)
+//			{
+//				if (i + e <= amount)
+//				{
+//					table[i + e] = table[i];
+//				}
+//			}
+//		}
+//	}
+//
+//	if (table[amount] == false)
+//	{
+//		return -1;
+//	}
+//	std::sort(coins.begin(), coins.end(), [](int i, int j)->bool {return j < i; });
+//
+//	
+//	int temp = amount;
+//	int result{};
+//	while (temp != 0)
+//	{
+//		for (auto c : coins)
+//		{
+//			if (temp - c >= 0)
+//			{
+//				if (table[temp - c] == true)
+//				{
+//					temp = temp - c;
+//					result++;
+//					break;
+//				}
+//			}
+//		}
+//	}
+//
+//	return result;
+//}
+//m*n
 int ConinChange(std::vector<int>& coins, const int amount)
 {
 	if (amount == 0)
@@ -13,111 +67,209 @@ int ConinChange(std::vector<int>& coins, const int amount)
 		return 0;
 	}
 
-	std::vector<bool> table(amount + 1, false);
-	table[0] = true;
+	std::vector<int> v(amount + 1, 9999);
+	v[0] = 0;
 
-	for (int i = 0; i <= amount; ++i)
+	for (auto coin : coins)
 	{
-		if (table[i])
+		for (int i = coin; i <= amount; i++)
 		{
-			for (auto e : coins)
-			{
-				if (i + e <= amount)
-				{
-					table[i + e] = table[i];
-				}
-			}
+			v[i] = std::min(v[i], v[i - coin] + 1);
 		}
 	}
-
-	if (table[amount] == false)
+	if (v[amount] == 9999)
 	{
 		return -1;
 	}
-	std::sort(coins.begin(), coins.end(), [](int i, int j)->bool {return j < i; });
-
-	int coin = coins[0];
-	int temp = amount;
-	int coinCont{};
-	while (temp != 0)
-	{
-		for (auto c : coins)
-		{
-			if (temp - c >= 0)
-			{
-				if (table[temp - c] == true)
-				{
-					temp = temp - c;
-					coinCont++;
-					break;
-				}
-			}
-		}
-	}
-
-	return coinCont;
+	return v[amount];
 }
+
+
+//m*n*n?
+//bool CanWordBreak(const std::string s, const std::vector<std::string>& wordDict)
+//{
+//	int size = s.size()+1;
+//	std::vector<bool> table(size, false);
+//	table[0] = true;
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		if (table[i])
+//		{
+//			for (auto e : wordDict)
+//			{
+//				if (i + (e.size()) < size)
+//				{
+//					if (i == s.find(e))
+//					{
+//						table[i + e.size()] = table[i];
+//					}
+//				}
+//				
+//			}
+//		}
+//	}
+//	return table[size-1];
+//}
+
+//bool CanWordBreak(const std::string s, const std::vector<std::string>& wordDict)
+//{
+//	
+//	std::vector<bool> table(s.size()+1, false);
+//	table[0] = true;
+//
+//	for (int i = 0; i <= s.size(); i++)
+//	{
+//		for (auto& word : wordDict)
+//		{
+//			if (i + word.size() <= s.size() && s.substr(i, word.size()) == word)
+//			{
+//				table[i + word.size()] = true;
+//			}
+//		}
+//	}
+//	return table[s.size()];
+//}
 
 bool CanWordBreak(const std::string s, const std::vector<std::string>& wordDict)
 {
-	int size = s.size()+1;
-	std::vector<bool> table(size, false);
-	table[0] = true;
-
-	for (int i = 0; i < size; i++)
+	std::unordered_set<std::string>set;
+	for (auto e : wordDict)
 	{
-		if (table[i])
+		set.insert(e);
+	}
+
+	std::string tempStr = s;
+	std::string resStr = s;
+
+	auto index = set.begin();
+
+	while (index != set.end())
+	{
+		std::string strs = *index;
+
+		int length = set.size();
+		int resLength = resStr.size();
+
+		std::string saveStr = resStr;
+
+		tempStr = resStr.substr(0, length);
+
+		if (length > resLength)
 		{
-			for (auto e : wordDict)
+			index++;
+			continue;
+		}
+		resStr = resStr.substr(length);
+
+		if (tempStr == strs)
+		{
+			resStr = resStr;
+			index = set.begin();
+			if (resStr.empty())
 			{
-				if (i + (e.size()) < size)
-				{
-					if (i == s.find(e))
-					{
-						table[i + e.size()] = table[i];
-					}
-				}
-				
+				return true;
 			}
 		}
+		else 
+		{
+			resStr = saveStr;
+			++index;
+		}
 	}
-	return table[size-1];
+	return false;
 }
+	
+
+
 
 int MaxProfit(const std::vector<int>& prices)
 {
-	
-	std::multimap<int, std::pair<int, int>> map;
-	int size = prices.size();
-	for (int i = 0; i< size; i++)
-	{
-		for (int j = i + 1; j < size; j++)
-		{
-			if (prices[j] - prices[i] > 0)
-			{
-				map.insert({ prices[j] - prices[i], {i,j} });
-			}
-		}
-	}
-	
-	return (std::max_element(map.begin(), map.end())->second.second) + 1;
+	if (prices.empty()) return 0; // 가격 배열이 비어있으면 이익은 0
 
+	int min_price = prices[0]; // 첫 번째 가격을 최소 가격으로 초기화
+	int max_profit = 0; // 최대 이익을 0으로 초기화
+
+	// 가격 배열의 두 번째 요소부터 끝까지 순회
+	for (size_t i = 1; i < prices.size(); ++i)
+	{
+		// 현재 이익을 계산하고 최대 이익을 갱신
+		max_profit = std::max(max_profit, prices[i] - min_price);
+
+		// 현재 가격을 이용해 최소 가격 갱신
+		min_price = std::min(min_price, prices[i]);
+	}
+
+	return max_profit; // 최대 이익 반환
 }
 
-int LengthOFLongestSubstring(std::string s)
+
+////n*n
+//int MaxProfit(const std::vector<int>& prices)
+//{
+//	
+//	std::multimap<int, std::pair<int, int>> map;
+//	int size = prices.size();
+//	for (int i = 0; i< size; i++)
+//	{
+//		for (int j = i + 1; j < size; j++)
+//		{
+//			if (prices[j] - prices[i] > 0)
+//			{
+//				map.insert({ prices[j] - prices[i], {i,j} });
+//			}
+//		}
+//	}
+//	
+//	return std::max_element(map.begin(), map.end())->first;
+//}
+// 
+//n
+int lengthOfLongestSubstring(const std::string& s) 
 {
-	std::string answer;
-	answer = s[0];
-	for (int i = 1; i < s.size(); i++)
+	int len = s.length();
+
+	if (len <= 1) 
 	{
-		if (answer.find(s[i]) == -1)
-		{
-			answer += s[i];
+		return len;
+	}
+
+	std::vector<int> repeat(128, 0);
+	int left = 0, right = 0;
+	repeat[s[left]]++;
+
+	int answer = 0;
+	while (true) {
+		if (right == len - 1) {
+			break;
+		}
+
+		if (right < len - 1 && repeat[s[right + 1]] == 0) {
+			repeat[s[++right]]++;
+			answer = std::max(answer, right - left + 1);
+		}
+		else {
+			repeat[s[left++]]--;
 		}
 	}
 
-	return answer.size();
+	return answer;
 }
+//int LengthOfLongestSubstring(std::string s)
+//{
+//	int answer{};
+//	for (int i{}; i < s.length(); i++)
+//	{
+//		int count{ 1 };
+//		while (s[i + 1] == s[i] + 1)
+//		{
+//			i++;
+//			count++;
+//		}
+//		if (answer < count) answer = count;
+//	}
+//	return answer;
+//}
 
 struct TreeNode
 {
@@ -128,10 +280,9 @@ struct TreeNode
 	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
-
+//n
 int MaxBTDepth(TreeNode* root)
 {
-	
 	if (root == nullptr)
 	{
 		return 0;
@@ -153,19 +304,19 @@ int MaxBTDepth(TreeNode* root)
 
 	return result;
 }
+
 int main()
 {
 
 	//std::vector<int> coins{ 1,3,5 };
 	//std::cout << ConinChange(coins, 11);
 
-	//std::cout << CanWordBreak("helloword", {"hello", "word"});
+	//std::cout << CanWordBreak("catsandog", {"cats", "dog", "sand", "and", "cat"});
 
-	//std::vector<int> x{ 7,1,5,3,6,4 };
+	//std::vector<int> x{ 7,6,4,3,1 };
 	//std::cout << MaxProfit(x);
 
-	//std::string s("abcabcbb");
-	//std::cout << LengthOFLongestSubstring(s);
+	std::string s("pwwkew");
+	std::cout << lengthOfLongestSubstring(s);
 
-	
 }
